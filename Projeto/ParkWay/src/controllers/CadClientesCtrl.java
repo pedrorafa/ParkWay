f3e212d55f428b1426e143d134f5ba4b68db527a
082150246@cefsa.corp.local <082150246@cefsa.corp.local>
2018-10-23 21:12:20 -0300

    Documentação atualizada

--- Projeto/ParkWay/src/controllers/CadClientesCtrl.java
+++ Projeto/ParkWay/src/controllers/CadClientesCtrl.java
@@ -4,15 +4,22 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class CadClientesCtrl {
     @FXML
     private AnchorPane childPane;
 
-	public void ShowCadVeiculo() throws IOException{
-        AnchorPane pnlOne = FXMLLoader.load(this.getClass().getResource("./ShowVeiculos.fxml"));
-        childPane.getChildren().setAll(pnlOne);
+	public void ShowAddCliente() throws IOException{
+        AnchorPane pnlOne = FXMLLoader.load(this.getClass().getResource("/application/AddClientes.fxml"));
+		Scene scene = new Scene(pnlOne);
+		
+		Stage popUp = new Stage();
+		popUp.setResizable(false);
+		popUp.setTitle("Adicionar Cliente");
+		popUp.show();
 	}
 	public static void main(String[] args) {
 	}
--- README.md
+++
@@ -1,2 +0,0 @@
-# ParkWay
-Projeto desenvolvido para auxiliar no controle de um estacionamento mensal de forma simples e rápida.
